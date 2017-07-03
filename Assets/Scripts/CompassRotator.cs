b71af5791bb69cc4988d6d32f998f6e6d2e1d955
yuutaro-O <yuutarousoft@gmail.com>
2017-07-04 00:16:43 +0900

    カメラの回転を方位に「大体」合うように
    カメラ回転などを方位に大体で合わせるように
    （10度おき程度の）回転にして、回転をマイルドに。

--- Assets/Scripts/CompassRotator.cs
+++ Assets/Scripts/CompassRotator.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class CompassRotator : MonoBehaviour {
 
     Transform rotationObject;
-
+    float rotation_Yaw;
+    [SerializeField]
+    uint rotationSplit_num = 36;
+    float rotationSplitDegrees;
 	// Use this for initialization
 	void Start () {
         rotationObject = gameObject.transform;
         Input.compass.enabled = true;
+        rotationSplitDegrees = (360f / rotationSplit_num);
     }
 	// Update is called once per frame
 	void Update () {
         //Input.location.Start();
-        rotationObject.parent.rotation = Quaternion.Euler(rotationObject.parent.rotation.x, Input.compass.trueHeading, rotationObject.parent.rotation.z);
+        rotation_Yaw = Input.compass.trueHeading;
+        if (rotationSplitDegrees == 0) rotation_Yaw = 0;
+        else rotation_Yaw = (Mathf.Floor(rotation_Yaw / rotationSplitDegrees) * rotationSplitDegrees);
+        rotationObject.parent.rotation = Quaternion.Euler(rotationObject.parent.rotation.x, rotation_Yaw, rotationObject.parent.rotation.z);
         //rotationObject.rotation = Quaternion.AngleAxis(Input.compass.magneticHeading, rotationObject.parent.position);
         
         //rotationObject.rotation = Quaternion.;
